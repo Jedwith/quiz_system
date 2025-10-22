e68b984357b8d4e6cf7b06ce6443be1d5c876ff3
Jedwith <Ega346@gmail.com>
2025-10-22 20:05:20 +0300

    Добавлена форма ввода Bearer токена в Swagger вместо OAuth2

--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -1,14 +1,20 @@
-from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import JWTError
 from src.core.security import verify_token
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
-def get_teacher_id(token: str = Depends(oauth2_scheme)) -> int:
-    payload = verify_token(token)
-    user_id = payload.get("sub")
-    role = payload.get("role")
+bearer_scheme = HTTPBearer()
 
-    if not user_id or role != "teacher":
-        raise HTTPException( status_code=403, detail="Доступ разрешён только преподавателям")
-    return int(user_id)
+
+async def get_teacher_id(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
+    token = credentials.credentials
+    try:
+        payload = verify_token(token)
+        user_id = payload.get("sub")
+        role = payload.get("role")
+        if not user_id or role != "teacher":
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Доступ разрешён только преподавателям")
+        return int(user_id)
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Неверный токен")
