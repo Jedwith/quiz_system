d0803c99407f830ff24b0159ca76af1c52679bfa
Jedwith <Ega346@gmail.com>
2025-10-29 22:34:28 +0300

    Реализованы методы для редактирования и удаления вопросов

--- src/services/question_service.py
+++ src/services/question_service.py
@@ -2,7 +2,7 @@ from src.models import Test, question
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.models.question import Question
 from src.models.question_test import QuestionTest
-from src.schemas.question import QuestionCreateInTest, QuestionAddToTest, QuestionInTestResponse
+from src.schemas.question import QuestionCreateInTest, QuestionAddToTest, QuestionUpdateInTest, QuestionInTestResponse
 from sqlalchemy import select
 from fastapi import HTTPException, status
 
@@ -71,7 +71,7 @@ async def add_existing_question_to_test(db: AsyncSession, question_data:Question
     question = result.scalar_one_or_none()
 
     if not question:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Тест не найден или у вас нет прав доступа")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Вопрос не найден или у вас нет прав доступа")
 
     result = await db.execute(select(QuestionTest).where(QuestionTest.test_id == test_id, QuestionTest.question_id == question_id))
     link = result.scalar_one_or_none()
@@ -95,3 +95,58 @@ async def add_existing_question_to_test(db: AsyncSession, question_data:Question
         order=link.order,
         points=link.points
     )
+
+async def update_question_in_test(db: AsyncSession, question_data:QuestionUpdateInTest, test_id:int, question_id:int, teacher_id:int):
+    result = await db.execute(select(Test).where(Test.id == test_id, Test.teacher_id == teacher_id))
+    test = result.scalar_one_or_none()
+
+    if not test:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Тест не найден или у вас нет прав доступа")
+
+    result = await db.execute(
+        select(Question, QuestionTest)
+        .join(QuestionTest, Question.id == QuestionTest.question_id)
+        .where(Question.id == question_id, QuestionTest.test_id == test_id)
+    )
+    data = result.one_or_none()
+    if not data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Вопрос не найден в указанном тесте")
+
+    question, link = data
+
+    if question_data.text is not None:
+        question.text = question_data.text
+    if question_data.type_id is not None:
+        question.type_id = question_data.type_id
+
+    if question_data.order is not None:
+        link.order = question_data.order
+    if question_data.points is not None:
+        link.points = question_data.points
+
+    await db.commit()
+    await db.refresh(question)
+    await db.refresh(link)
+    return QuestionInTestResponse(
+        id=question.id,
+        text=question.text,
+        type_id=question.type_id,
+        order=link.order,
+        points=link.points
+    )
+
+async def delete_question_from_test(db: AsyncSession, test_id:int, question_id:int, teacher_id:int):
+    result = await db.execute(select(Test).where(Test.id == test_id, Test.teacher_id == teacher_id))
+    test = result.scalar_one_or_none()
+
+    if not test:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Тест не найден или у вас нет прав доступа")
+
+    result = await db.execute(select(QuestionTest).where(QuestionTest.test_id == test_id, QuestionTest.question_id == question_id))
+    link = result.scalar_one_or_none()
+
+    if not link:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Вопрос не найден")
+    await db.delete(link)
+    await db.commit()
+
