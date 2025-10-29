8e2cb0f7d15b8b70395398a58d22e3e42b2ba15c
Jedwith <Ega346@gmail.com>
2025-10-29 23:26:01 +0300

    Создана схема для работы с ответами

---
+++ src/schemas/answer.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class AnswerCreate(BaseModel):
+    text: str
+    is_correct: bool = False
+
+class AnswerUpdate(BaseModel):
+    text: Optional[str] = None
+    is_correct: Optional[bool] = None
+
+class AnswerResponse(BaseModel):
+    id: int
+    name: str
+    order: int
+    is_correct: bool
+    question_id: int
+
+    class Config:
+        from_attributes = True
