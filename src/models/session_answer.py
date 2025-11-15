3d4c7a0b50513f9a02ad52395f922a03360e8164
Jedwith <Ega346@gmail.com>
2025-11-15 17:44:23 +0300

    Обновлены поля text и answer_id

--- src/models/session_answer.py
+++ src/models/session_answer.py
@@ -1,20 +1,20 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Integer, Boolean, DateTime, func, ForeignKey
+from sqlalchemy import Integer, String, Boolean, DateTime, func, ForeignKey
 from datetime import datetime
 from src.database import Base
 
 class SessionAnswer(Base):
     __tablename__ = 'session_answers'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, index=True)
-    text: Mapped[str]
+    text: Mapped[str | None] = mapped_column(String, )
     is_correct: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    answered_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default=func.now())
+    answered_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     session_id: Mapped[int] = mapped_column(Integer, ForeignKey('test_sessions.id'), index=True)
     question_id: Mapped[int] = mapped_column(Integer, ForeignKey('questions.id'), index=True)
     student_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), index=True)
-    answer_id: Mapped[int] = mapped_column(Integer, ForeignKey('answers.id'), index=True)
+    answer_id: Mapped[int | None] = mapped_column(Integer, ForeignKey('answers.id'), index=True)
 
-    student:Mapped["User"] = relationship("User", back_populates="answers_in_session")
-    session:Mapped["TestSession"] = relationship("TestSession", back_populates="answers_in_session")
-    question:Mapped["Question"] = relationship("Question", back_populates="answers_in_session")
-    answer:Mapped["Answer"] = relationship("Answer", back_populates="answers_in_session")
+    student: Mapped["User"] = relationship("User", back_populates="answers_in_session")
+    session: Mapped["TestSession"] = relationship("TestSession", back_populates="answers_in_session")
+    question: Mapped["Question"] = relationship("Question", back_populates="answers_in_session")
+    answer: Mapped["Answer"] = relationship("Answer", back_populates="answers_in_session")
