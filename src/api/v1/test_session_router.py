b8b9006101315e1a8eded2b2360f885d517a1ca3
Jedwith <Ega346@gmail.com>
2025-11-15 17:42:57 +0300

    Добавлен эндпонит для отправки ответа студентом

--- src/api/v1/test_session_router.py
+++ src/api/v1/test_session_router.py
@@ -3,8 +3,9 @@ from fastapi import APIRouter, Depends, Path, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import get_db
 from src.schemas.test_session import TestSessionCreate, TestSessionResponse
-from src.services import test_session_service
-from src.core.dependencies import get_teacher_id
+from src.schemas.session_answer import SessionAnswerCreate, SessionAnswerResponse
+from src.services import test_session_service, session_answer_service
+from src.core.dependencies import get_teacher_id, get_student_id
 
 router = APIRouter(prefix="/sessions", tags=["Test Sessions"])
 
@@ -15,3 +16,7 @@ async def create_test_session(session: TestSessionCreate, room_code: str, teache
 @router.post("/{session_id}/finish", status_code=status.HTTP_200_OK, summary="Завершение сессии")
 async def finish_test_session(session_id: int, teacher_id: int = Depends(get_teacher_id), db: AsyncSession = Depends(get_db)):
     return await test_session_service.finish_test_session(db, session_id, teacher_id)
+
+@router.post("/{session_id}/answers", response_model=SessionAnswerResponse, summary="Отправка ответа студентом в сессии")
+async def submit_answer(session_id: int, answer_data: SessionAnswerCreate, student_id: int = Depends(get_student_id), db: AsyncSession = Depends(get_db)):
+    return await session_answer_service.save_student_answer(db, answer_data, session_id, student_id)
